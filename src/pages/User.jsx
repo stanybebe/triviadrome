a7324afff9c3e00db1a315233711bf145dc85744
stanybebe <tristan.e.whitehill@gmail.com>
2023-06-04 16:06:53 -0400

    make it better

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('https://triviadrome.herokuapp.comsocket.io/?EIO=4&transport=polling'); // Replace with your server URL
+const socket = io('https://triviadrome.herokuapp.com/socket.io/?EIO=4&transport=polling'); // Replace with your server URL
 
 function User() {
   const [username, setUsername] = useState('');
