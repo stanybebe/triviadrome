52db5ec59d28fe5d77ad6c2078447e50994f74d6
stanybebe <tristan.e.whitehill@gmail.com>
2023-06-06 10:39:47 -0400

    yyyyy

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -9,7 +9,7 @@ function Admin() {
   const [joinedUsers, setJoinedUsers] = useState([]);
   const [selectedUserId, setSelectedUserId] = useState('');
 
-  const sockets = io("https://triviadrome.herokuapp.com");
+  const sockets = io("https://triviadrome.herokuapp.com/");
 
 
   useEffect(() => {
