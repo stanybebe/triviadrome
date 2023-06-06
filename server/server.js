9fdf2ad46d013eb9be3064b58097512b56295ce9
stanybebe <tristan.e.whitehill@gmail.com>
2023-06-06 10:19:27 -0400

    trying my best to get sockets to see anything

--- server/server.js
+++ server/server.js
@@ -34,65 +34,61 @@ const io = socketIO(server, {
 });
 
 
+io.on('getJoinedUsers', (username) => {
+   
+  if (joinedUsers.includes(username)){
+    socket.emit('joinError', 'Username is already taken');
+    return;
+  }
 
+
+      const user = {
+        id: socket.id,
+        username: username,
+      };
+
+      joinedUsers.push(user);
+
+
+     
+  io.emit('joinedUsers', joinedUsers);
+  console.log(joinedUsers);
+
+});
+
+io.on('userMessage', (data) => {
+
+  console.log('Received user message:', data);
+  const userId = data.username;
+  const message = data.message;
+  console.log(id);
+  console.log(message);
+  
+  io.emit('adminMessage', { userId: String(userId), message: String(message) });
+});
+
+io.on('adminMessageOut', (data) => {
+  console.log('Received admin message:',  data);
+
+
+
+      io.emit('adminMessageIn', data);
+    
+  
+
+});
 // Socket.IO configuration
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
-  const id = socket.id;
-  
-  socket.on('getJoinedUsers', (username) => {
-   
-    if (joinedUsers.includes(username)){
-      socket.emit('joinError', 'Username is already taken');
-      return;
-    }
-
-
-        const user = {
-          id: socket.id,
-          username: username,
-        };
-
-        joinedUsers.push(user);
-  
-  
-       
-    io.emit('joinedUsers', joinedUsers);
-    console.log(joinedUsers);
-
-  });
-//////////////////////////////////////////////////////////
-  socket.on('userMessage', (data) => {
-
-    console.log('Received user message:', data);
-    const userId = data.username;
-    const message = data.message;
-    console.log(id);
-    console.log(message);
-    
-    io.emit('adminMessage', { userId: String(userId), message: String(message) });
-  });
-
-  
-//////////////////////////////////////////////////////////
-  socket.on('adminMessageOut', (data) => {
-    console.log('Received admin message:',  data);
 
   
 
-        io.emit('adminMessageIn', data);
-      
-    
-
-  });
-
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected:', socket.id);
-    const index = joinedUsers.indexOf(socket.id);
-    if (index !== -1) {
-      joinedUsers.splice(index, 1);
-    }
-  });
 });
 
+io.on('disconnect', () => {
+  console.log('A user disconnected:', socket.id);
+  const index = joinedUsers.indexOf(socket.id);
+  if (index !== -1) {
+    joinedUsers.splice(index, 1);
+  }
+});
